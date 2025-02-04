7c2c2a24b07823534e00e70135aa6c6ebcd7864a
Ananya Ratakonda <aratakonda@ucdavis.edu>
2025-02-04 15:37:06 -0800

    starting it

--- app/(pages)/about-us/page.jsx
+++ app/(pages)/about-us/page.jsx
@@ -3,7 +3,13 @@ import styles from './page.module.scss';
 export default function About() {
     return (
       <main>
-        <div>About</div>
+        <div className={styles.page}>
+          <h1>Clinic Roles</h1>
+          <h3>The incredible team that makes JVMC happen</h3>
+          <div className={styles.card}>
+            <p></p>
+          </div>
+        </div>
       </main>
     );
   }
