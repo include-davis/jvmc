4fa47cf0139c7cdf9e6889a6961e8124cabbe244
yooian <yooian03@gmail.com>
2025-02-04 15:49:33 -0800

    Add card styling and Image functionality

--- app/(pages)/_components/VolunteerCard/VolunteerCard.jsx
+++ app/(pages)/_components/VolunteerCard/VolunteerCard.jsx
@@ -1,9 +1,25 @@
 import styles from "./VolunteerCard.module.scss";
+import Image from "next/image";
 
-export default function VolunteerCard({ title, titleIcon, mainText, image, link, linkText }) {
+export default function VolunteerCard({ direction, image, titleIcon, title, mainText, link, linkText }) {
     return (
-      <div className={styles.card}>
-        hi
+      <div className={styles.cardComponent}>
+        <div className={styles.card}>
+          <div className={styles.cardText}>
+            <div className={styles.cardTitle}>
+              Title
+            </div>
+            <div className={styles.cardBody}>
+              bodybodybody<br/>bodybodybody<br/>bodybodybody<br/>bodybodybody
+            </div>
+            <div className={styles.cardLink}>
+              link
+            </div>
+          </div>
+        </div>
+        <div className={styles.cardImage}>
+          <Image src="/images/magpie.png" alt="temp" layout="fill" objectFit="cover"/>
+        </div>
       </div>
     );
 }
