cd4e3fcad70a632519978931efa021ba99df88b6
yooian <yooian03@gmail.com>
2025-02-03 23:01:49 -0800

    Create initial test styling

--- app/(pages)/_components/VolunteerCard/VolunteerCard.jsx
+++ app/(pages)/_components/VolunteerCard/VolunteerCard.jsx
@@ -2,7 +2,7 @@ import styles from "./VolunteerCard.module.scss";
 
 export default function VolunteerCard({ title, titleIcon, mainText, image, link, linkText }) {
     return (
-      <div className="styles.card">
+      <div className={styles.card}>
         hi
       </div>
     );
--- app/(pages)/volunteer/page.jsx
+++ app/(pages)/volunteer/page.jsx
@@ -1,10 +1,12 @@
 import styles from './page.module.scss';
+import VolunteerCard from '../_components/VolunteerCard/VolunteerCard';
 
 export default function Volunteer() {
     return (
       <main>
         <div>Volunteer</div>
         {/* VolunteerCard components */}
+        <VolunteerCard></VolunteerCard>
       </main>
     );
   }
