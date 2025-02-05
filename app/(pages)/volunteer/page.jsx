c16799913fbbfcc44b7d54480278a581d786d240
Yash Pradhan <yashpradhan181@gmail.com>
2025-02-04 16:06:23 -0800

    added bottom learn more section outline

--- app/(pages)/volunteer/page.jsx
+++ app/(pages)/volunteer/page.jsx
@@ -3,7 +3,11 @@ import styles from './page.module.scss';
 export default function Volunteer() {
     return (
       <main>
-        <div>Volunteer</div>
+        <div className={styles.learn_more}></div>
+          <h1 className={styles.header}>Interested in learning more about each role?</h1>
+          <div className={styles.text}>For more information on each volunteering role, please navigate to the About Us page. There you will find out more about what each role in the clinic entails.</div>
+          <button className={styles.buttn}>ABOUT US</button>
+          {/*button className={styles.buttn} onClick={handle_click}></button>*/}
       </main>
     );
   }
