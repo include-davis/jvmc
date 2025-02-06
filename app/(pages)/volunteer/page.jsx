2942b3ab7f8255581ab421ed82cd7364b6af8552
Yash Pradhan <yashpradhan181@gmail.com>
2025-02-05 21:36:53 -0800

    created flexbox for bottom. Figma accurate layout, slight visual irregularities

--- app/(pages)/volunteer/page.jsx
+++ app/(pages)/volunteer/page.jsx
@@ -3,11 +3,14 @@ import styles from './page.module.scss';
 export default function Volunteer() {
     return (
       <main>
-        <div className={styles.learn_more}></div>
+        <div className={styles.learn_more}>
           <h1 className={styles.header}>Interested in learning more about each role?</h1>
-          <div className={styles.text}>For more information on each volunteering role, please navigate to the About Us page. There you will find out more about what each role in the clinic entails.</div>
-          <button className={styles.buttn}>ABOUT US</button>
+          <div className={styles.text}>For more information on each volunteering role, please navigate to the <span className={styles.aboutUs}>About Us</span> page. There you will find out more about what each role in the clinic entails.</div>
+          <button className={styles.buttn}>
+            <div className={styles.buttnText}>About Us</div>
+          </button>
           {/*button className={styles.buttn} onClick={handle_click}></button>*/}
+        </div>
       </main>
     );
   }
