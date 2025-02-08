c312a4f1af73b4e8bc64b141bf30a3f09fe59f5c
sareeyali <sayli@ucdavis.edu>
2025-02-07 21:47:35 -0800

    started

--- app/(pages)/about-us/page.jsx
+++ app/(pages)/about-us/page.jsx
@@ -1,10 +1,13 @@
 import styles from './page.module.scss';
 
 export default function About() {
-    return (
-      <main>
-        <div>About</div>
-      </main>
-    );
-  }
-  
+  return (
+    <>
+      <div className={styles.aboutUs}>About Us</div>
+      <div className={styles.phrase}>
+      Serving the community one patient at a time.
+      </div>
+    </>
+    
+  );
+}
