df2f85b712ed70c6a8e14bf320084d0b91b53e38
Jonathan Ngo <jonngo@ucdavis.edu>
2025-02-07 19:03:19 -0800

    just to pull

--- app/(pages)/_components/Header/Header.jsx
+++ app/(pages)/_components/Header/Header.jsx
@@ -7,8 +7,8 @@ export default function Header() {
       <div className={styles.header}>
           <Image src="/images/JVMC Logo 1.png" alt="JVMC Logo" width={118} height={89} className={styles.headerImage} />
           <div className={styles.headerLinks}>
-              <a href="#about" className={styles.headerItems}>Home</a>
-              <a href="#skills" className={styles.headerItems}>Clinic Schedules</a>
+              <a href="#home" className={styles.headerItems}>Home</a>
+              <a href="../../../clinic-schedule/page.jsx" className={styles.headerItems}>Clinic Schedules</a>
               <a href="#projects" className={styles.headerItems}>Community Partner</a>
               <a href="#projects" className={styles.headerItems}>About Us</a>
               <a href="#projects" className={styles.headerItems}>Volunteer</a>
