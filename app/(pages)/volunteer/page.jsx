cbf3276fdf261ca22e230326218224f957ee4f92
Yash Pradhan <yashpradhan181@gmail.com>
2025-02-10 22:19:52 -0800

    added globals and started storing text data in JSON

--- app/(pages)/volunteer/page.jsx
+++ app/(pages)/volunteer/page.jsx
@@ -1,14 +1,17 @@
 import styles from './page.module.scss';
+import text_data from '@/app/(pages)/_data/volunteer.json';
 
 export default function Volunteer() {
+
+
     return (
       <main>
+        <h1 className={styles.title}>{text_data.Title}</h1>
+
         <div className={styles.learn_more}>
-          <h1 className={styles.header}>Interested in learning more about each role?</h1>
-          <div className={styles.text}>For more information on each volunteering role, please navigate to the <span className={styles.aboutUs}>About Us</span> page. There you will find out more about what each role in the clinic entails.</div>
-          <button className={styles.buttn}>
-            <div className={styles.buttnText}>About Us</div>
-          </button>
+          <h1 className={styles.header}>{text_data.bottom_title}</h1>
+          <p className={styles.text}>For more information on each volunteering role, please navigate to the <span className={styles.aboutUs}>About Us</span> page. There you will find out more about what each role in the clinic entails.</p>
+          <div className="btn">{text_data.bottom_button}</div>
           {/*button className={styles.buttn} onClick={handle_click}></button>*/}
         </div>
       </main>
