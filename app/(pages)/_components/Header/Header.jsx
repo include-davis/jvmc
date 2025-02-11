5e0fcb5c6ef33bf06fd380486d45915073e5155a
Jonathan Ngo <jonngo@ucdavis.edu>
2025-02-10 18:45:57 -0800

    finished header may need some changes

--- app/(pages)/_components/Header/Header.jsx
+++ app/(pages)/_components/Header/Header.jsx
@@ -1,19 +1,21 @@
 import styles from "./Header.module.scss";
 import Image from 'next/image';
-
+import Link from "next/link";
 
 export default function Header() {
   return (
       <div className={styles.header}>
           <Image src="/images/JVMC Logo 1.png" alt="JVMC Logo" width={118} height={89} className={styles.headerImage} />
           <div className={styles.headerLinks}>
-              <a href="#home" className={styles.headerItems}>Home</a>
-              <a href="../../../clinic-schedule/page.jsx" className={styles.headerItems}>Clinic Schedules</a>
-              <a href="#projects" className={styles.headerItems}>Community Partner</a>
-              <a href="#projects" className={styles.headerItems}>About Us</a>
-              <a href="#projects" className={styles.headerItems}>Volunteer</a>
-              {/* change the contact us later */}
-              <a href="#projects" className={styles.headerContact}>Contact Us</a>
+            {/* idk how to link to home */}
+              <Link href="/" className={styles.headerItems}>Home</Link>
+              <Link href="/clinic-schedule" className={styles.headerItems}>Clinic Schedules</Link>
+              <Link href="/community-partners" className={styles.headerItems}>Community Partner</Link>
+              <Link href="/about-us" className={styles.headerItems}>About Us</Link>
+              <Link href="/volunteer" className={styles.headerItems}>Volunteer</Link>
+                {/* using global button but it doesnt seem correct */}
+              {/* <a href="#projects" className="btn" style={{ textDecoration: 'none' }}>Contact Us</a> */}
+              <Link href="/contact-us" className={styles.headerContact}>Contact Us</Link>
           </div>
       </div>
   );
