fa3647c63086c88cdc24949c34f0538101e39184
Yash Pradhan <yashpradhan181@gmail.com>
2025-02-11 00:24:38 -0800

    finished layout for top and bottom. TODO: add image, button, clean code

--- app/(pages)/volunteer/page.jsx
+++ app/(pages)/volunteer/page.jsx
@@ -7,6 +7,15 @@ export default function Volunteer() {
     return (
       <main>
         <h1 className={styles.title}>{text_data.Title}</h1>
+        <h4 className={styles.sub_title}>{text_data.subTitle}</h4>
+
+        <div className={styles.opening}>
+          <div className={styles.landing_photo}></div>
+          <div className={styles.intro}>
+            <p>{text_data.intro_1}</p>
+            <p>{text_data.intro_2}</p>
+          </div>
+        </div>
 
         <div className={styles.learn_more}>
           <h1 className={styles.header}>{text_data.bottom_title}</h1>
