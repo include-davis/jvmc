f61f10db5391b6a7d334dfa8d3b1e3737211d8a4
Yash Pradhan <yashpradhan181@gmail.com>
2025-02-11 15:48:23 -0800

    switched margins to paddings, width is now % not px, implemented image, all text is in json. TODO: fix the button and check all figma-code accuracies

--- app/(pages)/volunteer/page.jsx
+++ app/(pages)/volunteer/page.jsx
@@ -1,28 +1,40 @@
 import styles from './page.module.scss';
-import text_data from '@/app/(pages)/_data/volunteer.json';
+import Link from 'next/link';
+import Image from 'next/image'
+import text from '@/app/(pages)/_data/volunteer.json';
 
 export default function Volunteer() {
 
-
     return (
       <main>
-        <h1 className={styles.title}>{text_data.Title}</h1>
-        <h4 className={styles.sub_title}>{text_data.subTitle}</h4>
+        <h1 className={styles.title}>{text.Title}</h1>
+        <h4 className={styles.sub_title}>{text.subTitle}</h4>
 
         <div className={styles.opening}>
-          <div className={styles.landing_photo}></div>
+          <Image
+            className={styles.landing_photo}
+            src={text.entrance_pic}
+            width={521}
+            height={370}
+            alt={text.entrance_pic_descript}
+          />
           <div className={styles.intro}>
-            <p>{text_data.intro_1}</p>
-            <p>{text_data.intro_2}</p>
+            <p>{text.intro_1}</p>
+            <p>{text.intro_2}</p>
           </div>
         </div>
-
-        <div className={styles.learn_more}>
-          <h1 className={styles.header}>{text_data.bottom_title}</h1>
-          <p className={styles.text}>For more information on each volunteering role, please navigate to the <span className={styles.aboutUs}>About Us</span> page. There you will find out more about what each role in the clinic entails.</p>
-          <div className="btn">{text_data.bottom_button}</div>
-          {/*button className={styles.buttn} onClick={handle_click}></button>*/}
-        </div>
+        <div className={styles.bottom}>
+          <div className={styles.learn_more}>
+            <h1 className={styles.header}>{text.bottom_title}</h1>
+            <p className={styles.text}>
+              {text.bottom_text["chunk1"]} 
+              <span className={styles.aboutUs}>{text.bottom_text["chunk2"]}</span>
+              {text.bottom_text["chunk3"]}
+            </p>
+            <Link href="/about-us" className="btn" >{text.bottom_button}</Link>
+            {/*button className={styles.buttn} onClick={handle_click}></button>*/}
+          </div>
+        </div>  
       </main>
     );
   }
