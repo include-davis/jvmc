1c33c6622caa79a4c9c8a424fc2e20dfe737b9e9
Jonathan Ngo <jonngo@ucdavis.edu>
2025-02-11 15:27:34 -0800

    finished header

--- app/(pages)/_components/Header/Header.jsx
+++ app/(pages)/_components/Header/Header.jsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 export default function Header() {
   return (
       <div className={styles.header}>
-          <Image src="/images/JVMC Logo 1.png" alt="JVMC Logo" width={118} height={89} className={styles.headerImage} />
+          <Image src="/images/JVMC Logo 1.png" alt="JVMC Logo" width={120} height={88} className={styles.headerImage} />
           <div className={styles.headerLinks}>
               <Link href="/" className={styles.headerItems}>Home</Link>
               <Link href="/clinic-schedule" className={styles.headerItems}>Clinic Schedules</Link>
               <Link href="/community-partners" className={styles.headerItems}>Community Partner</Link>
               <Link href="/about-us" className={styles.headerItems}>About Us</Link>
               <Link href="/volunteer" className={styles.headerItems}>Volunteer</Link>
-              <Link href="/contact-us" className={styles.headerContact}>Contact Us</Link>
+              <Link href="/contact-us" className={"btn"}>Contact Us</Link>
           </div>
       </div>
   );
