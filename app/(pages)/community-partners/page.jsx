45b51c348df3096e5877b2b6de4231c421221caa
misty360 <akathiresan@ucdavis.edu>
2025-02-11 14:47:13 -0800

    changed titles

--- app/(pages)/community-partners/page.jsx
+++ app/(pages)/community-partners/page.jsx
@@ -4,6 +4,8 @@ export default function CommunityPartners() {
     return (
       <main>
         <div>Community Partners</div>
+        <h1 className={styles.titleText}> Community Partners</h1>
+        <p className={styles.subtitleText}>Our trusted allies supporting our mission beyond the our clinics.</p>
       </main>
     );
   }
