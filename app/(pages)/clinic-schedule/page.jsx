b5bb3b3501172ef64cfd914bada480f2842a5cd1
kanikaram-sejal <kanikaramsejal@gmail.com>
2025-02-11 15:57:11 -0800

    styled with css

--- app/(pages)/clinic-schedule/page.jsx
+++ app/(pages)/clinic-schedule/page.jsx
@@ -9,18 +9,34 @@ export default function ClinicSchedule() {
             <h3>Need an appointment? Check availability and schedule one with our clincs now.</h3>
           </div>
           <div className={styles.gcal}>
+            <div>
 
+            </div>
+            <div className={styles.key}>
+              <h3>Calendar Key</h3>
+              <div className={styles.cir1}></div>
+              <p>Reproductive Health Clinic</p>
+              <div className={styles.cir2}></div>
+              <p>JVMC Clinic</p>
+              <div className={styles.cir3}></div>
+              <p>Gender Health Center</p>
+            </div>
           </div>
           <div className={styles.infocards}>
             <div className={styles.jvmc}>
               <h4>JVMC Clinic</h4>
+              <p>JVMC Clinic provides free primary health care services to individuals using intravenous drugs, work in the sex trade, or identify as transgender.</p>
+  
+              <h4>Walk-ins only</h4>
             </div>
-            <div className={styles.rhc}>
+            {/* <div className={styles.rhc}>
               <h4>Reproductive Health Clinic</h4>
+              <p>The Reproductive Health Clinic provides sexual and reproductive clinical care such as IUDs, STI testing/treatment, and menstrual products.</p>
             </div>
             <div className={styles.ghc}>
               <h4>Gender Health Center</h4>
-            </div>
+              <p>GHC offers free or low-cost health, legal, and support services for trans and gender-expansive communities.</p>
+            </div> */}
           </div>
         </div>
       </main>
