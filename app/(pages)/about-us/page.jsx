61378a323f10047346bb16d6ce0a173411c5f081
Said Gonzalez Lopez <99686473+SaidGLopez12@users.noreply.github.com>
2025-02-11 15:41:02 -0800

    Saved current progress. Starting to work on the json and positioning of elements. Went over the card section. Shift from background-image usage to next.js image

--- app/(pages)/about-us/page.jsx
+++ app/(pages)/about-us/page.jsx
@@ -2,50 +2,21 @@ import styles from './page.module.scss';
 import HomePageCard from '@/app/HomePageCard/HomePageCard';
 
 
-// card_content = [
-//   {
-//     title: "test 1",
-//     icon:"cross icon",
-//     text: ["Hello", "Hola",],
-//   },
-//   {
-//     title: "test 2",
-//     icon:"people icon",
-//     text: ["Goodbye", "Adios",]
-    
-//   },
-
-// ];
-
 export default function About() {
     return (
       <main>
-        <div className = {styles.body}>
+        <div className = "body">
+          <div className = {styles.tempNavbar}>
+            <h1>Temp Navbar</h1>
+          </div>
           <div className = {styles.title_body}>
-            <h1> Welcome to Joan Viteri Medical Clinic </h1>
-            <h4>Serving health care needs of uninsured drug users, sex workers, <br></br> trans folks, and their families.</h4>
-            <button className = {styles.btn}>Contact Us</button>
+            <h1 className> Welcome to Joan Viteri Memorial Clinic </h1>
+            <h4 className> Serving health care needs of uninsured drug users, sex workers, <br></br> trans folks, and their families.</h4>
+            <button className ="btn">Contact Us</button>
           </div>
 
           <div className = {styles.homepage_body}>
-          <h4> Serving the Oak Park Community </h4>
-
-          {/* <div className = {styles.HomePageCard}>
-          {card_content.map((card_content,idx) => {
-            return <div key={idx}>
-                <HomePageCard card_content={card_content}></HomePageCard>
-            </div>
-          })}
-          </div> */}
-
-          {/* <div className={styles.interestCard}>
-          {interests.map((interest, idx) => {
-              return <div key={idx}>
-                    <InterestCard interest={interest}></InterestCard>
-                </div>
-          }) }
-        
-        </div> */}
+          <h4 className = "h4"> Serving the Oak <br></br> Park Community </h4>
 
 
           </div>
