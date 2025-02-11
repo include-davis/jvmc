58bcffa771a0be6afa3bc0c65da174bf9f663b20
Jonathan Ngo <jonngo@ucdavis.edu>
2025-02-10 18:48:35 -0800

    just removed random comments

--- app/(pages)/_components/Header/Header.jsx
+++ app/(pages)/_components/Header/Header.jsx
@@ -7,14 +7,11 @@ export default function Header() {
       <div className={styles.header}>
           <Image src="/images/JVMC Logo 1.png" alt="JVMC Logo" width={118} height={89} className={styles.headerImage} />
           <div className={styles.headerLinks}>
-            {/* idk how to link to home */}
               <Link href="/" className={styles.headerItems}>Home</Link>
               <Link href="/clinic-schedule" className={styles.headerItems}>Clinic Schedules</Link>
               <Link href="/community-partners" className={styles.headerItems}>Community Partner</Link>
               <Link href="/about-us" className={styles.headerItems}>About Us</Link>
               <Link href="/volunteer" className={styles.headerItems}>Volunteer</Link>
-                {/* using global button but it doesnt seem correct */}
-              {/* <a href="#projects" className="btn" style={{ textDecoration: 'none' }}>Contact Us</a> */}
               <Link href="/contact-us" className={styles.headerContact}>Contact Us</Link>
           </div>
       </div>
