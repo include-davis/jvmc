a70618d8ff7e15b3d7a635ba9a3f85baaa39ef8f
Ananya Ratakonda <aratakonda@ucdavis.edu>
2025-02-15 21:11:15 -0800

    Finished for now, however might need to change the sizing so I dm the designer and might change the sizes

--- app/(pages)/about-us/page.jsx
+++ app/(pages)/about-us/page.jsx
@@ -9,40 +9,52 @@ export default function About() {
         <div className={styles.page}>
           <h1>{text.mainHeader}</h1>
           <h4>{text.mainSubheader}</h4>
+        
             <div className={styles.card}>
-              <div className={styles.cardTop}>
+              
               <Image
-                src={text.iconImages["studentImage"]}
-                width={39}
-                height={39} >
-                  
+              className={styles.img} src={text.photos["undergrad"]} width={211} height={211} alt="undergradImage"> 
+              </Image>
+              <div className={styles.cardTop}>
+                <Image 
+                src={text.iconImages["studentImage"]} width={39} height={39} alt="iconStudent">
                 </Image>
                 <h3>{text.subheaders["undergrad"]}</h3>
               </div>
               <p> {text.description["undergrad"]} </p>
             </div>
             <div className={styles.card2}>
-              <div className={styles.cardTop}>
-              <Image src={text.iconImages["suitcaseImg"]} width={39} height={39} > 
-
+              <Image 
+              className={styles.img} src={text.photos["graduate"]} width={211} height={211} alt="gradImage">
               </Image>
+              <div className={styles.cardTop}>
+                <Image 
+                src={text.iconImages["suitcaseImg"]} width={39} height={39} alt="iconSuitcase"> 
+                </Image>
                 <h3>{text.subheaders["graduate"]}</h3>
               </div>
               <p> {text.description["graduate"]} </p>
             </div>
             <div className={styles.card}>
-              <div className={styles.cardTop}>
-              <Image src={text.iconImages["heartImg"]} width={39} height={40} > 
+              <Image
+                className={styles.img2} src={text.photos["social"]} width={211} height={211} alt="socialWorkImage">
               </Image>
+              <div className={styles.cardTop}>
+                <Image 
+                src={text.iconImages["heartImg"]} width={39} height={40} alt="iconHeart"> 
+                </Image>
                 <h3>{text.subheaders["social"]}</h3>
               </div>
               <p> {text.description["social"]} </p>
             </div>
             <div className={styles.card2}>
-              <div className={styles.cardTop}>
-              <Image src={text.iconImages["doctorImg"]} width={39} height={39} > 
-
+              <Image
+              className={styles.img2} src={text.photos["preceptor"]} width={211} height={211} alt="preceptorImage">
               </Image>
+              <div className={styles.cardTop}>
+                <Image 
+                  src={text.iconImages["doctorImg"]} width={39} height={39} alt="iconDoctor" > 
+                </Image>
                 <h3>{text.subheaders["preceptor"]}</h3>
               </div>
               <p> {text.description["preceptor"]} </p>
