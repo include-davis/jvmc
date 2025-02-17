d629a51e30f30c2a08bfd65141bdb1313781a0b5
Said Gonzalez Lopez <99686473+SaidGLopez12@users.noreply.github.com>
2025-02-17 13:20:49 -0800

    Switched to home and started to add more data in the json file as well as the structure for the home page

--- app/(pages)/(home)/page.jsx
+++ app/(pages)/(home)/page.jsx
@@ -1,12 +1,26 @@
 import styles from './page.module.scss';
-// import HomePageCard from '@/app/HomePageCard/HomePageCard';
+import HomeData from '@/app/(pages)/_data/home.json';
 
 export default function Home() {
   return (
     <main>
-        <div>Home</div>
-         {/* - Home Page Card Components Go Here - */} 
+        <div className = "body">
 
+            <div className = {styles.titleWithButton}>
+                  <h1> {HomeData.homePageTitle} </h1>
+                  <h4> {HomeData.homePageSubtitle} </h4>
+                  <button className ="btn"> {HomeData.buttonMsg} </button>
+            </div>
+
+            <div className = {styles.mainCardSection}>
+              <div className={styles.mainCardSectionTitle}>
+                <h4>{HomeData.mainCardSection_Title}</h4>
+              </div>
+
+            </div>
+
+        </div>
+         
     </main>
   );
 }
