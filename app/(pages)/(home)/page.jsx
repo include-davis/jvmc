0e4b6bde3792355c7ab75ac1ac55197ab98abc9e
Said Gonzalez Lopez <99686473+SaidGLopez12@users.noreply.github.com>
2025-02-17 13:52:32 -0800

    Added Card Section and starting to work on the images and styling

--- app/(pages)/(home)/page.jsx
+++ app/(pages)/(home)/page.jsx
@@ -1,5 +1,6 @@
 import styles from './page.module.scss';
 import HomeData from '@/app/(pages)/_data/home.json';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -15,6 +16,33 @@ export default function Home() {
             <div className = {styles.mainCardSection}>
               <div className={styles.mainCardSectionTitle}>
                 <h4>{HomeData.mainCardSection_Title}</h4>
+              </div>
+              <div className = {styles.cards_Section}>
+                  <div className = {styles.card_1}>
+                    {/* left side has image and right side has text */}
+                    <div className = {styles.card_1_ImageBox}>
+                    <Image
+                      src={HomeData.image1_Link}
+                      alt="Image Text" 
+                      width={300}  // Width of the image
+                      height={400} // Height of the image
+                    />
+                      
+                    </div>
+                    <div className = {styles.card_1_TextBox}>
+                    
+
+                    </div>
+
+                    
+
+
+                    
+                  </div>
+                  <div className = {styles.card_2}>
+                    {/* left side has text and right side has image */}
+
+                  </div>
               </div>
 
             </div>
