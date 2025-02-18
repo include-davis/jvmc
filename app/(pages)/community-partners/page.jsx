7c523bca7b64454aa406cd237d70231a67eee749
Yash Pradhan <yashpradhan181@gmail.com>
2025-02-18 15:50:02 -0800

    carousel page divisions structure set up

--- app/(pages)/community-partners/page.jsx
+++ app/(pages)/community-partners/page.jsx
@@ -1,9 +1,38 @@
 import styles from './page.module.scss';
 
+import Image from 'next/image'
+import text from '@/app/(pages)/_data/community-partners.json';
+
 export default function CommunityPartners() {
     return (
       <main>
         <div>Community Partners</div>
+        <h2>{text.bottom_title}</h2>
+        <h4>{text.bottom_subtitle}</h4>
+
+        <div class={styles.carousel}>
+
+          <div class={styles.row}>
+
+            <div class={styles.column}>
+              <Image></Image>
+            </div>
+            <div class={styles.column}>
+              <Image></Image>
+            </div>
+            <div class={styles.column}>
+              <Image></Image>
+            </div>
+            <div class={styles.column}>
+              <Image></Image>
+            </div>
+            <div class={styles.column}>
+              <Image></Image>
+            </div>
+
+          </div>
+
+        </div>
       </main>
     );
   }
