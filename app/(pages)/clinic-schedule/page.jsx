4f0a502f6c3ee68e7fb796e88e6a0d953955294a
kanikaram-sejal <kanikaramsejal@gmail.com>
2025-02-17 23:15:14 -0800

    Fixed info cards

--- app/(pages)/clinic-schedule/page.jsx
+++ app/(pages)/clinic-schedule/page.jsx
@@ -1,44 +1,60 @@
+"use client"; 
 import styles from './page.module.scss';
+import data from '../_data/clinic-schedule.json';
 
 export default function ClinicSchedule() {
-    return (
-      <main>
-        <div className={styles.page}>
-          <div className={styles.header}>
-            <h1>Calendar & Clinic Schedules</h1>
-            <h3>Need an appointment? Check availability and schedule one with our clincs now.</h3>
-          </div>
-          <div className={styles.gcal}>
-            <div>
 
-            </div>
-            <div className={styles.key}>
-              <h3>Calendar Key</h3>
-              <div className={styles.cir1}></div>
-              <p>Reproductive Health Clinic</p>
-              <div className={styles.cir2}></div>
-              <p>JVMC Clinic</p>
-              <div className={styles.cir3}></div>
-              <p>Gender Health Center</p>
-            </div>
+  return (
+    <main>
+      <div className={styles.page}>
+        <div className={styles.header}>
+          <h2>Calendar & Clinic Schedules</h2>
+          <h4>Need an appointment? Check availability and schedule one with our clincs now.</h4>
+        </div>
+
+        <div className={styles.gcal}>
+          <div>
+
           </div>
-          <div className={styles.infocards}>
-            <div className={styles.jvmc}>
-              <h4>JVMC Clinic</h4>
-              <p>JVMC Clinic provides free primary health care services to individuals using intravenous drugs, work in the sex trade, or identify as transgender.</p>
-  
-              <h4>Walk-ins only</h4>
-            </div>
-            {/* <div className={styles.rhc}>
-              <h4>Reproductive Health Clinic</h4>
-              <p>The Reproductive Health Clinic provides sexual and reproductive clinical care such as IUDs, STI testing/treatment, and menstrual products.</p>
-            </div>
-            <div className={styles.ghc}>
-              <h4>Gender Health Center</h4>
-              <p>GHC offers free or low-cost health, legal, and support services for trans and gender-expansive communities.</p>
-            </div> */}
+          <div className={styles.key}>
+            <h4>Calendar Key</h4>
+            {Object.entries(data.key).map(([key, clinic]) => (
+              <div key={key} className={styles.keyElem}>
+                <div className={styles.circ} style={{backgroundColor: `var(${clinic.color})`}}></div>
+                <p>{clinic.title}</p>
+              </div>
+            ))}
+            {/* <div className={styles.cir1}></div>
+            <p>Reproductive Health Clinic</p>
+            <div className={styles.cir2}></div>
+            <p>JVMC Clinic</p>
+            <div className={styles.cir3}></div>
+            <p>Gender Health Center</p> */}
           </div>
         </div>
-      </main>
-    );
-  }
+
+        <div className={styles.infocards}>
+          {Object.entries(data.info).map(([key, clinic]) => (
+            <div key={key} className={styles.card}>
+              <div className={styles.info}>
+                <h4>{clinic.title}</h4>
+                <p>{clinic.paragraph}</p>
+                <div className={styles.tm}>
+                  {/* <img src={'../public/mdi_calendar.png'}></img> */}
+                  <p>{clinic.time}</p>
+                  {/* <img></img> */}
+                  <p>{clinic.message}</p>
+                </div>
+              </div>
+              {clinic.appt === "button" ? (
+                <button className={styles.btn}>Visit GHC Website</button>
+              ) : (
+                <p className={styles.appt}>{clinic.appt}</p>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+}
