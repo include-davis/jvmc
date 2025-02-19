dcf1e391fadecf7cc30a16285b57c701db380b76
Ananya Ratakonda <aratakonda@ucdavis.edu>
2025-02-18 16:12:26 -0800

    fixed the code by adding the component

--- app/(pages)/about-us/page.jsx
+++ app/(pages)/about-us/page.jsx
@@ -1,20 +1,29 @@
 import styles from './page.module.scss';
-import { PiStudent } from "react-icons/pi";
 import Image from "next/image"
 import text from '@/app/(pages)/_data/about-us.json';
+import ClinicRolesCard from '../_components/ClinicRolesCard/ClinicRolesCard';
+
 
 export default function About() {
     return (
-      <main>
+     
+     /*<main>
         <div className={styles.page}>
-          <h1>{text.mainHeader}</h1>
-          <h4>{text.mainSubheader}</h4>
+          <div className={styles.header}>
+              <h1>{text.mainHeader}</h1>
+              <h4>{text.mainSubheader}</h4>
+          </div>
         
             <div className={styles.card}>
               
-              <Image
-              className={styles.img} src={text.photos["undergrad"]} width={211} height={211} alt="undergradImage"> 
-              </Image>
+              <div className={styles.img}>
+          <Image
+            src={img}
+            style={{ objectFit: "fill" }}
+            fill={true}
+            alt={imgAlt}
+          />
+        </div>
               <div className={styles.cardTop}>
                 <Image 
                 src={text.iconImages["studentImage"]} width={39} height={39} alt="iconStudent">
@@ -25,7 +34,7 @@ export default function About() {
             </div>
             <div className={styles.card2}>
               <Image 
-              className={styles.img} src={text.photos["graduate"]} width={211} height={211} alt="gradImage">
+              className={styles.img} src={text.photos["graduate"]} width={211} height={211} alt= {text.photoAlts["Graduates"]}>
               </Image>
               <div className={styles.cardTop}>
                 <Image 
@@ -62,6 +71,32 @@ export default function About() {
           
         </div>
       </main>
+      */
+
+      <main>
+        <div className={styles.page}>
+          <div className={styles.header}>
+              <h1>{text.mainHeader}</h1>
+              <h4>{text.mainSubheader}</h4>
+          </div>
+          {text.iconImages.map((_, index) => 
+            <ClinicRolesCard
+              key={index}
+              icon={text.iconImages[index]}
+              iconAlt={text.iconAlts[index]}
+              img={text.photos[index]}
+              imgAlt={text.photoAlts[index]}
+              align={text.alignments[index]}
+              subheader={text.subheaders[index]}
+              description={text.description[index]}
+            />
+          )}
+
+          </div>
+         </main>
+
+     
     );
   }
-  
+
+ 
