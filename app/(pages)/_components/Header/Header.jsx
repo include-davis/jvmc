4b714125313b8e24d8d5bec41d470166e4f3e309
Jonathan Ngo <jonngo@ucdavis.edu>
2025-02-20 13:34:00 -0800

    Added cristian's changes, json and css changes but havent done link states (still a bit confused on that)

--- app/(pages)/_components/Header/Header.jsx
+++ app/(pages)/_components/Header/Header.jsx
@@ -1,19 +1,19 @@
 import styles from "./Header.module.scss";
-import Image from 'next/image';
+import Image from "next/image";
 import Link from "next/link";
+import navLinks from "../../_data/header.json"; 
 
 export default function Header() {
   return (
-      <div className={styles.header}>
-          <Image src="/images/JVMC Logo 1.png" alt="JVMC Logo" width={120} height={88} className={styles.headerImage} />
-          <div className={styles.headerLinks}>
-              <Link href="/" className={styles.headerItems}>Home</Link>
-              <Link href="/clinic-schedule" className={styles.headerItems}>Clinic Schedules</Link>
-              <Link href="/community-partners" className={styles.headerItems}>Community Partner</Link>
-              <Link href="/about-us" className={styles.headerItems}>About Us</Link>
-              <Link href="/volunteer" className={styles.headerItems}>Volunteer</Link>
-              <Link href="/contact-us" className={"btn"}>Contact Us</Link>
-          </div>
+    <div className={styles.header}>
+      <Image src="/images/JVMC Logo 1.png" alt="JVMC Logo" width={120} height={88} />
+      <div className={styles.headerLinks}>
+        {navLinks.map(({ href, label, isButton }) => (
+          <Link key={href} href={href} className={isButton ? "btn" : styles.headerItems}>
+            {isButton ? label : <p className={styles.headerItems}>{label}</p>}
+          </Link>
+        ))}
       </div>
+    </div>
   );
 }
