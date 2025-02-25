6dc1dd563a0baca988ca0dcd1f09f5b78ad5de9e
Jonathan Ngo <jonngo@ucdavis.edu>
2025-02-24 20:28:11 -0800

    fixing the changes, this branch is only the contact card

--- app/(pages)/_components/Header/Header.jsx
+++ app/(pages)/_components/Header/Header.jsx
@@ -1,32 +1,9 @@
-"use client"; // Ensure this is a client component
-
-import { usePathname } from "next/navigation"; 
-import Image from "next/image";
-import Link from "next/link";
-import navLinks from "../../_data/header.json";
 import styles from "./Header.module.scss";
 
 export default function Header() {
-  const pathname = usePathname(); 
-
-  return (
-    <div className={styles.header}>
-      <Image src="/images/JVMC Logo 1.png" alt="JVMC Logo" width={120} height={88} />
-      <div className={styles.headerLinks}>
-        {navLinks.map(({ href, label, isButton }) => {
-          const isActive = pathname === href; 
-
-          return (
-            <Link
-              key={href}
-              href={href}
-              className={`${isButton ? "btn" : styles.headerItems} ${isActive ? styles.headerItemActive : ""}`}
-            >
-              {label}
-            </Link>
-          );
-        })}
+    return (
+      <div>
+        Header
       </div>
-    </div>
-  );
+    );
 }
