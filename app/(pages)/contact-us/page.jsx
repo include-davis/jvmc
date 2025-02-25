0845603c6761b284a1003fb7139ecd8de8a88a67
yooian <yooian03@gmail.com>
2025-02-25 00:07:17 -0800

    Add centering and text color

--- app/(pages)/contact-us/page.jsx
+++ app/(pages)/contact-us/page.jsx
@@ -3,17 +3,19 @@ import styles from './page.module.scss';
 export default function ContactUs() {
     return (
       <main>
-        <div>Contact Us</div>
-        {/* MAP COMPONENT */}
-        {/* SOCIAL MEDIA COMPONENT */}
-        {/* QUESTIONS? COMPONENT */}
-        <div className={styles.questionsCard}>
-          <h3>Questions?</h3>
-          <h3>We would love to hear!</h3>
-          <p></p>
-          <p></p>
+        <div className={styles.contactPage}>
+          <div>Contact Us</div>
+          {/* MAP COMPONENT */}
+          {/* SOCIAL MEDIA COMPONENT */}
+          {/* QUESTIONS? COMPONENT */}
+          <div className={styles.questionsCard}>
+            <h3>Questions?</h3>
+            <h3>We would love to hear!</h3>
+            <p>Please <b>do not use this page for inquiries</b></p>
+            <p>For questions about volunteering please email </p>
+          </div>
+          {/* CONTACT FORM COMPONENT */}
         </div>
-        {/* CONTACT FORM COMPONENT */}
       </main>
     );
   }
