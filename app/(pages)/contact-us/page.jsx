b013539cb75ef76ec3af4b627af2fecc85649ac4
yooian <yooian03@gmail.com>
2025-02-25 00:20:03 -0800

    Update link color and bolding

--- app/(pages)/contact-us/page.jsx
+++ app/(pages)/contact-us/page.jsx
@@ -12,7 +12,7 @@ export default function ContactUs() {
             <h3>Questions?</h3>
             <h3>We would love to hear!</h3>
             <p>Please <b>do not use this page for inquiries</b></p>
-            <p>For questions about volunteering please email </p>
+            <p>For questions about volunteering please email <b><a href="mailto:jvmcvolunteer@gmail.com">jvmcvolunteer@gmail.com</a></b></p>
           </div>
           {/* CONTACT FORM COMPONENT */}
         </div>
