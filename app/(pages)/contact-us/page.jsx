d5274062c72e888216e4bd5965ecac7d49556c03
yooian <yooian03@gmail.com>
2025-02-25 00:38:17 -0800

    Add JSON support and move non-styled text to contact-us.json

--- app/(pages)/contact-us/page.jsx
+++ app/(pages)/contact-us/page.jsx
@@ -1,6 +1,8 @@
 import styles from './page.module.scss';
+import contactData from '../_data/contact-us.json';
 
 export default function ContactUs() {
+  const { heading1, heading2 } = contactData.questionsCard
     return (
       <main>
         <div className={styles.contactPage}>
@@ -9,8 +11,8 @@ export default function ContactUs() {
           {/* SOCIAL MEDIA COMPONENT */}
           {/* QUESTIONS? COMPONENT */}
           <div className={styles.questionsCard}>
-            <h3>Questions?</h3>
-            <h3>We would love to hear!</h3>
+            <h3>{heading1}</h3>
+            <h3>{heading2}</h3>
             <p>Please <b>do not use this page for inquiries</b></p>
             <p>For questions about volunteering please email <b><a href="mailto:jvmcvolunteer@gmail.com">jvmcvolunteer@gmail.com</a></b></p>
           </div>
