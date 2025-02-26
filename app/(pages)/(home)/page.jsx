0bf91f43e5d47c7809c358867395e36a888e7909
Jonathan Ngo <jonngo@ucdavis.edu>
2025-02-26 15:35:48 -0800

    added naomi's changes, change color and rid of component's static height

--- app/(pages)/(home)/page.jsx
+++ app/(pages)/(home)/page.jsx
@@ -9,27 +9,27 @@ export default function Home() {
         <div className={styles.contactCard}>
           <div className={styles.infoContainer}>
             <div className={styles.info}>
-              <FaHome size={32}/>
+              <FaHome color="var(--emerald)" size={32}/>
               <div className={styles.infoText}>
-                <h2>Come Visit Us!</h2>
-                <p>3647 40th St, Sacramento, CA 95817</p>
+                <h3 style={{ color: "var(--emerald)" }}>Come Visit Us!</h3>
+                <p style={{ color: "var(--dark-grey)" }}>3647 40th St, Sacramento, CA 95817</p>
               </div>
             </div>
             <div className={styles.info}>
-              <FaClock size={32}/>
+              <FaClock color="var(--emerald)" size={32}/>
               <div className={styles.infoText}>
-                <h2>Opening Hours</h2>
-                <p>Every Saturday: 1:00pm - 4:00pm</p>
+                <h3 style={{ color: "var(--emerald)" }}>Opening Hours</h3>
+                <p style={{ color: "var(--dark-grey)" }}>Every Saturday: 1:00pm - 4:00pm</p>
               </div>
             </div>
             <div className={styles.info}>
-              <FaEnvelope size={32}/>
-              <h2>Contact</h2>
+              <FaEnvelope color="var(--emerald)" size={32}/>
+              <h3 style={{ color: "var(--emerald)" }}>Contact</h3>
             </div>
             <Link href="/contact-us" className={"btn"}>Ask Us Questions</Link>
           </div>
           <div className={styles.imageContainer}>
-            <div className={styles.gradientCircle}></div> {/* Gradient circle behind */}
+            <div className={styles.gradientCircle}></div> 
             <Image src="/images/IMG_8158 2.png" alt="Contact Photo" width={360} height={280} className={styles.image} />
           </div>
         </div>
