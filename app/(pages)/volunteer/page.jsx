2017029b7dd63ae95b659408ebce3c32d43636c7
C3viche <28328104+C3viche@users.noreply.github.com>
2025-03-04 15:49:04 -0800

    some alignment fixes

--- app/(pages)/volunteer/page.jsx
+++ app/(pages)/volunteer/page.jsx
@@ -6,23 +6,27 @@ import text from '@/app/(pages)/_data/volunteer.json';
 export default function Volunteer() {
 
     return (
-      <main>
-        <h1 className={styles.title}>{text.title}</h1>
-        <h4 className={styles.sub_title}>{text.sub_title}</h4>
-
-        <div className={styles.opening}>
-          <Image
-            className={styles.landing_photo}
-            src={text.entrance_pic}
-            width={521}
-            height={370}
-            alt={text.entrance_pic_descript}
-          />
-          <div className={styles.intro}>
-            <p>{text.intro_1}</p>
-            <p>{text.intro_2}</p>
+      <main className={styles.page}>
+        <div className={styles.top}>
+          <div className={styles.header}>
+            <h1 className={styles.title}>{text.title}</h1>
+            <h4 className={styles.sub_title}>{text.sub_title}</h4>
+          </div>
+          <div className={styles.opening}>
+            <Image
+              className={styles.landing_photo}
+              src={text.entrance_pic}
+              width={521}
+              height={370}
+              alt={text.entrance_pic_descript}
+            />
+            <div className={styles.intro}>
+              <p>{text.intro_1}</p>
+              <p>{text.intro_2}</p>
+            </div>
           </div>
         </div>
+
         <div className={styles.bottom}>
           <div className={styles.learn_more}>
             <h4 className={styles.bottom_title}>{text.bottom_title}</h4>
