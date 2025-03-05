fca0e533e1d6a5ada74a96200e456138b9ec2889
C3viche <28328104+C3viche@users.noreply.github.com>
2025-03-04 21:04:45 -0800

    fixed active states + button and moved some info to json

--- app/(pages)/_components/Footer/Footer.jsx
+++ app/(pages)/_components/Footer/Footer.jsx
@@ -3,14 +3,11 @@ import styles from "./Footer.module.scss";
 import Link from "next/link";
 import Image from "next/image";
 import { FaInstagram } from "react-icons/fa";
-import footerData from "../../_data/footer.json";
+import data from "../../_data/footer.json";
 import { usePathname } from "next/navigation";
 
 export default function Footer() {
   const pathname = usePathname(); 
-
-  const { platform, url, icon } = footerData.socialLinks;
-  const { home, clinic, community, about, volunteer, contact } = footerData.navLinks;
 
   return (
     <footer className={styles.footer}>
@@ -18,16 +15,16 @@ export default function Footer() {
         <div className={styles.logoSection}>
           <div className={styles.logo}>
             <Image 
-              src="/logo.png" 
-              alt="JVMC Logo" 
+              src={data.img} 
+              alt={data.imgAlt} 
               style={{ objectFit: "fill" }} 
               fill={true} 
             />
           </div>
           <ul className={styles.socialLinks}>
-          <li className={styles.boxIcon}>
+            <li className={styles.boxIcon}>
               <a 
-                href="https://www.instagram.com/jvmclinic"
+                href={data.socialUrl}
                 target="_blank"
                 rel="noopener noreferrer"
                 >
@@ -35,24 +32,20 @@ export default function Footer() {
               </a>
             </li>
             <li className={styles.boxText}>
-              <div className={styles.at1}>@jvmclinic</div>
-              <div className={styles.at2}>@jvmcstudents</div>
+              <p>{data.mainInstagram}</p>
+              <p>{data.studentInstagram}</p>
             </li>
           </ul>
         </div>
         <ul className={styles.navLinks}>
-          {[
-            { label: "Home", url: "/" },
-            { label: "Clinic Schedules", url: "/clinic-schedule" },
-            { label: "Community Partners", url: "/community-partners" },
-            { label: "About Us", url: "/about-us" },
-            { label: "Volunteer", url: "/volunteer" },
-            { label: "Contact Us", url: "/contact-us" },
-          ].map(({ label, url }) => {
-            const isActive = pathname === url;
+          {data.navLinks.map(({ label, href, isButton }, idx) => {
+            const isActive = pathname === href;
             return (
-              <li key={url}>
-                <Link href={url} className={isActive ? styles.active : ""}>
+              <li key={idx}>
+                <Link 
+                  href={href}
+                  key={idx}
+                  className={isButton ? `${"btn"} ${isActive && styles.btnActive}` : `${styles.navLink} ${isActive && styles.active}`}>
                   {label}
                 </Link>
               </li>
