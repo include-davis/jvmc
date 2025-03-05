29c35fa4fc9f0c67ff9d220dce99a5f1c1023ef5
Sky Zhao <skyzhao04@gmail.com>
2025-03-04 16:13:33 -0800

    implement new design for footer

--- app/(pages)/_components/Footer/Footer.jsx
+++ app/(pages)/_components/Footer/Footer.jsx
@@ -1,12 +1,10 @@
-'use client'
+"use client";
 import styles from "./Footer.module.scss";
 import Link from "next/link";
 import Image from "next/image";
-import { FaInstagram} from "react-icons/fa";
-import { LuFacebook } from "react-icons/lu";
-import footerData from '../../_data/footer.json';
+import { FaInstagram } from "react-icons/fa";
+import footerData from "../../_data/footer.json";
 import { useRouter } from "next/navigation";
-
 
 export default function Footer() {
   const { platform, url, icon } = footerData.socialLinks;
@@ -18,29 +16,32 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.topSection}>
         <div className={styles.logoSection}>
-        <div className={styles.logo}>
-            <Image
-              src={"/logo.png"}
-              alt={"JVMC Logo"}
-              style={{ objectFit: "fill" }}
-              fill={true}
+          <div className={styles.logo}>
+            <Image 
+              src="/logo.png" 
+              alt="JVMC Logo" 
+              style={{ objectFit: "fill" }} 
+              fill={true} 
             />
           </div>
           <ul className={styles.socialLinks}>
-            <li>
-              <a href="https://www.instagram.com/jvmclinic" target="_blank" rel="noopener noreferrer">
-                <FaInstagram />
+            <li className={styles.boxIcon}>
+              <a 
+                href="https://www.instagram.com/jvmclinic"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+              <FaInstagram className={styles.icon} />
               </a>
             </li>
-            <li>
-              <a href="https://www.facebook.com/jvmclinic" target="_blank" rel="noopener noreferrer">
-                <LuFacebook />
-              </a>
+            <li className={styles.boxText}>
+              <div className={styles.at1}>@jvmcclinic</div>
+              <div className={styles.at2}>@jvmcstudents</div>
             </li>
           </ul>
         </div>
         <ul className={styles.navLinks}>
-        {[
+          {[
             { label: "Home", url: "/" },
             { label: "Clinic Schedules", url: "/clinic-schedule" },
             { label: "Community Partners", url: "/community-partners" },
