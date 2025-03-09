b0260034839f8981cb435bbace1ac570979d654e
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2025-03-08 18:50:48 -0800

    Add clinic roles gradient

--- app/(pages)/about-us/page.jsx
+++ app/(pages)/about-us/page.jsx
@@ -47,6 +47,15 @@ export default function About() {
               description={card.description}
             />
           )}
+          <div className={styles.clinicRolesGradientContainer}>
+            <div className={styles.clinicRolesGradient}>
+              <Image
+                src={data.clinic_roles_gradient}
+                alt={data.clinic_roles_gradient_alt}
+                fill={true}
+              />
+            </div>
+          </div>
         </div>
 
         <div className={styles.fairCard}>
