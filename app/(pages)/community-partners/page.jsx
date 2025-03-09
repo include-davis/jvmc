041e112090888a8699c1df6012e7b8b7e6709114
C3viche <28328104+C3viche@users.noreply.github.com>
2025-03-09 13:26:10 -0700

    moved carousel to its own component

--- app/(pages)/community-partners/page.jsx
+++ app/(pages)/community-partners/page.jsx
@@ -1,43 +1,17 @@
 import styles from './page.module.scss';
-
+import data from '@/app/(pages)/_data/community-partners.json';
 import Image from 'next/image'
 import text from '@/app/(pages)/_data/community-partners.json';
+import CommunityPartnersCarousel from '../_components/CommunityPartnersCarousel/CommunityPartnersCarousel';
 
 export default function CommunityPartners() {
-    const images = [  /*double images to give infinite scroll illusion */
-      text.carousel_images["img_1"],
-      text.carousel_images["img_2"],
-      text.carousel_images["img_3"],
-      text.carousel_images["img_4"],
-      text.carousel_images["img_1"],
-      text.carousel_images["img_2"],
-      text.carousel_images["img_3"],
-      text.carousel_images["img_4"],
-    ];
-
-    const imagesToDisplay = [...images];
-
     return (
       <main>
         <div>Community Partners</div>
         <h2 className={styles.community_partners_title}>{text.bottom_title}</h2>
         <h4 className={styles.community_partners_subtitle}>{text.bottom_subtitle}</h4>
 
-        <div className={styles.carousel} style={{ '--num-images': images.length / 2 }}>
-          <div className={styles.row}>
-            {imagesToDisplay.map((image, index) => (
-              <div key={index} className={styles.column}>
-                <Image
-                  className={styles.img}
-                  src={image}
-                  alt={`Image ${index}`}
-                  fill={true}
-                  style={{ objectFit: 'cover' }}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
+        <CommunityPartnersCarousel images={data.carousel_images} />
 
       </main>
     );
