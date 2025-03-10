42fc85d0c5369c92f0ebcc4c4583705ed06e517b
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2025-03-10 14:44:57 -0700

    Fix gradient z-index interaction issues

---
+++ app/(pages)/_components/CommunityPartnersCard/CommunityPartnersCard.jsx
@@ -0,0 +1,52 @@
+"use client";
+import { ImLocation } from "react-icons/im";
+import { IoMdCalendar } from "react-icons/io";
+import { FaCircleExclamation } from "react-icons/fa6";
+import styles from './CommunityPartnersCard.module.scss';
+import Image from "next/image";
+
+export default function CommunityPartnersCard({index, partner, containerClass}) {
+    return (
+        <div key={index} className={containerClass}>
+            <div className={styles.imageWrapper}>
+                <div className={styles.imageWrapper}>
+                <Image 
+                    src={partner.image_src} 
+                    style={{objectFit: 'cover'}}
+                    fill={true}
+                    alt={partner.images_src} 
+                />
+                </div>
+            </div>
+
+            <div className={styles.partnersCard}>
+                <div> 
+                <h4 className={styles.partnersName}>{partner.name}</h4>
+                <p className={styles.partnersDescription}>{partner.description}</p>
+                </div>
+                <div className={styles.contactInfo}>
+                <p className={styles.locationText}>
+                    <ImLocation className={styles.icon}/>
+                    {partner.location}
+                </p>
+                <p className={styles.dateText}>
+                    <IoMdCalendar className={styles.icon}/>
+                    {partner.date}
+                </p>
+                <p className={styles.appointmentText}>
+                    {index === 1 && (
+                    <FaCircleExclamation className={styles.icon}/>
+                    )}
+                    {partner.appointment}
+                </p>
+                </div>
+                <a
+                className="btn"
+                href={partner.website}
+                >
+                {partner.buttonText}
+                </a>
+            </div>
+        </div>
+    );
+}
