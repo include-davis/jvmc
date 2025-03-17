130088af8f91d37a482a3ac33b2f33860a0bd82e
C3viche <28328104+C3viche@users.noreply.github.com>
2025-03-17 13:26:37 -0700

    bring changes from cms integration

--- app/(pages)/(home)/page.jsx
+++ app/(pages)/(home)/page.jsx
@@ -2,186 +2,156 @@ import styles from "./page.module.scss";
 import Image from "next/image";
 import { FaHome, FaClock, FaEnvelope } from "react-icons/fa";
 import Link from "next/link";
-import data from '@/app/(pages)/_data/home.json'
-import HomepageCarousel from '../_components/HomepageCarousel/HomepageCarousel';
+import data from "@/app/(pages)/_data/home.json";
+import HomepageCarousel from "../_components/HomepageCarousel/HomepageCarousel";
+import HomeCard from "../_components/HomeCard/HomeCard";
 
-export default function Home() {
-  const { Sentence_1, Sentence_2, Sentence_3 } = data.text_Block1;
-  const { bulletpoint_1, bulletpoint_2, bulletpoint_3, bulletpoint_4 } =
-    data.text_Block2;
+import HomeGeneralInfoFallbackData from "@/app/(pages)/_data/general-info.json";
+import HomeCardsFallbackData from "@/app/(pages)/_data/home-cards.json";
+
+export async function getHomeGeneralInfo() {
+  return {
+    tagline: HomeGeneralInfoFallbackData.tagline,
+    address: HomeGeneralInfoFallbackData.address,
+    hours: HomeGeneralInfoFallbackData.hours,
+  };
+}
+
+export async function getHomeCards() {
+  return HomeCardsFallbackData;
+}
+
+export default async function Home() {
+  const homeData = await getHomeCards();
+  const generalData = await getHomeGeneralInfo();
 
   return (
-    <main>
-      <div className={styles.body}>
-        <div className={styles.titleWithButtonSection}>
-          <div className={styles.headerGradientContainer}>
-            <div className={styles.headerGradient}>
-              <Image
-                src={data.header_gradient}
-                alt={data.header_gradient_alt}
-                fill={true}
-              />
-            </div>
-          </div>
-          <div className={styles.teddyImage}>
-          <Image
-            src={data.teddy}
-            alt={data.teddyAlt}
-            width={166}
-            height={123}
-          />
-          </div>
-          <div className={styles.titleAndSubtitleBox}>
-            <h1> {data.homePageTitle} </h1>
-            <p> {data.homePageSubtitle} </p>
-          </div>
-
-          <div className={styles.contactButton}>
-            <Link className="btn" href="/contact-us">
-              {data.buttonMsg}
-            </Link>
-          </div>
-        </div>
-
-        <div className={styles.cardSectionBox}>
-          <div className={styles.cardSectionHeader}>
-            <h3>{data.cardSectionHeaderTitle}</h3>
+    <main className={styles.body}>
+      <div className={styles.titleWithButtonSection}>
+        <div className={styles.headerGradientContainer}>
+          <div className={styles.headerGradient}>
             <Image
-              src={data.cardSectionHeaderImage}
-              alt={data.cardSectionHeaderImageAlt}
-              width={780} // 780
-              height={280} // 280
-            />
-          </div>
-
-          <div className={styles.card}>
-            {/* left side has image and right side has text */}
-            <div className={styles.card_ImageBox}>
-              <Image
-                src={data.card1_Image_Link}
-                alt={data.card1_Image_Alt}
-                width={320}
-                height={424}
-                className={styles.image_1}
-              />
-            </div>
-
-            <div className={styles.card_TextBox}>
-              <div className={styles.card_TitleAndLogo}>
-                {/* <CiMedicalCross className = {styles.icon}/> */}
-                <Image
-                  src={data.health_icon}
-                  alt={data.health_icon_alt}
-                  width={40}
-                  height={40}
-                  className={styles.icon}
-                />
-                <h3>{data.card1_Title}</h3>
-              </div>
-
-              <ul className={styles.description_1}>
-                <li>{Sentence_1}</li>
-                <li>{Sentence_2}</li>
-                <li>{Sentence_3}</li>
-              </ul>
-            </div>
-          </div>
-
-          <div className={styles.dots1}>
-            <Image src={data.dots_1} alt={"dots"} width={259} height={533} />
-          </div>
-          <div className={styles.card}>
-            {/* left side has text and right side has image */}
-            <div className={styles.card_TextBox}>
-              <div className={styles.card_TitleAndLogo}>
-                {/* <BsPeople className = {styles.icon}/> */}
-                <Image
-                  src={data.biPeople_icon}
-                  alt={data.biPeople_icon_alt}
-                  width={40}
-                  height={40}
-                  // className={styles.icon}
-                />
-                <h3>{data.card2_Title}</h3>
-              </div>
-
-              <p>{data.card2_Subtitle}</p>
-
-              <ul className={styles.description_2}>
-                {data.text_Block2_Subtext}
-                <li>{bulletpoint_1}</li>
-                <li>{bulletpoint_2}</li>
-                <li>{bulletpoint_3}</li>
-                <li>{bulletpoint_4}</li>
-              </ul>
-            </div>
-
-            <div className={styles.card_ImageBox}>
-              <Image
-                src={data.card2_Image_Link}
-                alt={data.card2_Image_Alt}
-                width={305}
-                height={383}
-                className={styles.image_2}
-              />
-            </div>
-          </div>
-
-          <div className={styles.dots2}>
-            <Image src={data.dots_2} alt={"dots"} width={394} height={609} />
-          </div>
-        </div>
-
-        <HomepageCarousel data={data.carousel}/>
-
-        <div className={styles.contactCard}>
-          <div className={styles.dots3}>
-              <Image src={data.dots_3} alt={"dots"} width={461} height={545} />
-          </div>
-          <div className={styles.contactInfoContainer}>
-            <div className={styles.contactInfo} style={{gap: "20px"}}>
-              <FaHome color="var(--emerald)" size={40} />
-              <div className={styles.contactInfoText}>
-                <h3>{data.contactCard.visit.text}</h3>
-                <p>{data.contactCard.visit.address}</p>
-              </div>
-            </div>
-            <div className={styles.contactInfo} style={{paddingLeft: "4px"}}>
-              <FaClock color="var(--emerald)" size={32} />
-              <div className={styles.contactInfoText}>
-                <h3>{data.contactCard.hours.text}</h3>
-                <p>{data.contactCard.hours.hours}</p>
-              </div>
-            </div>
-            <div className={styles.contactInfo} style={{paddingLeft: "4px"}}>
-              <FaEnvelope color="var(--emerald)" size={32} />
-              <h3>{data.contactCard.contact}</h3>
-            </div>
-            <Link href={data.contactCard.button.link} className={"btn"}>
-              {data.contactCard.button.text}
-            </Link>
-          </div>
-          <div className={styles.contactImageContainer}>
-            <div className={styles.gradientCircle}></div>
-            <div className={styles.contactImage}>
-              <Image
-                src={data.contactCard.img.src}
-                alt={data.contactCard.img.alt}
-                style={{ objectFit: "cover" }}
-                fill={true}
-              />
-            </div>
-          </div>
-        </div>
-        {/* Hello */}
-        <div className={styles.bottomGradientContainer}>
-          <div className={styles.bottomGradient}>
-            <Image
-              src={data.bottom_gradient}
-              alt={data.bottom_gradient_alt}
+              src={"/images/homeGradient.png"}
+              alt={"Header Gradient"}
               fill={true}
             />
           </div>
+        </div>
+        <Image
+          src={"/images/jvmc-teddy.png"}
+          alt={"JVMC Teddy Bear"}
+          width={166}
+          height={123}
+        />
+        <div className={styles.titleAndSubtitleBox}>
+          <h1> Welcome to Joan Viteri Memorial Clinic </h1>
+          <p> {generalData.tagline} </p>
+        </div>
+
+        <div className={styles.contactButton}>
+          <Link className="btn" href="/contact-us">
+            Contact Us
+          </Link>
+        </div>
+      </div>
+
+      <div className={styles.cardSectionBox}>
+        <div className={styles.cardSectionHeader}>
+          <h3>Serving the Oak Park Community</h3>
+          <Image
+            src={"/subtitleheader.png"}
+            alt={"Image of Background for Card Section Header Title"}
+            width={780} // 780
+            height={280} // 280
+          />
+        </div>
+
+        {homeData.map((card, idx) => (
+          <HomeCard
+            key={idx}
+            title={card.title}
+            description={card.description}
+            img={card.image}
+            imgAlt={card.image_alt_text}
+            icon={card.icon}
+            iconAlt={card.icon_alt_text}
+            align={idx % 2 === 0 ? "left" : "right"}
+          />
+        ))}
+
+        <div className={styles.dots1}>
+          <Image
+            src={"/images/dots1.png"}
+            alt={"dots"}
+            width={259}
+            height={533}
+          />
+        </div>
+
+        <div className={styles.dots2}>
+          <Image
+            src={"/images/dots2.png"}
+            alt={"dots"}
+            width={394}
+            height={609}
+          />
+        </div>
+      </div>
+
+      <HomepageCarousel data={data.carousel} />
+
+      <div className={styles.contactCard}>
+        <div className={styles.dots3}>
+          <Image
+            src={"/images/dots3.png"}
+            alt={"dots"}
+            width={461}
+            height={545}
+          />
+        </div>
+        <div className={styles.contactInfoContainer}>
+          <div className={styles.contactInfo} style={{ gap: "20px" }}>
+            <FaHome color="var(--emerald)" size={40} />
+            <div className={styles.contactInfoText}>
+              <h3>Come Visit Us!</h3>
+              <p>{generalData.address}</p>
+            </div>
+          </div>
+          <div className={styles.contactInfo} style={{ paddingLeft: "4px" }}>
+            <FaClock color="var(--emerald)" size={32} />
+            <div className={styles.contactInfoText}>
+              <h3>Opening Hours</h3>
+              <p>{generalData.hours}</p>
+            </div>
+          </div>
+          <div className={styles.contactInfo} style={{ paddingLeft: "4px" }}>
+            <FaEnvelope color="var(--emerald)" size={32} />
+            <h3>Contact Us</h3>
+          </div>
+          <Link href={data.contactCard.button.link} className={"btn"}>
+            Ask Us Questions
+          </Link>
+        </div>
+        <div className={styles.contactImageContainer}>
+          <div className={styles.gradientCircle}></div>
+          <div className={styles.contactImage}>
+            <Image
+              src={data.contactCard.img.src}
+              alt={data.contactCard.img.alt}
+              style={{ objectFit: "cover" }}
+              fill={true}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.bottomGradientContainer}>
+        <div className={styles.bottomGradient}>
+          <Image
+            src={"/images/homeBottomGradient.png"}
+            alt={"Bottom Gradient"}
+            fill={true}
+          />
         </div>
       </div>
     </main>
---
+++ app/(pages)/_components/HomeCard/HomeCard.jsx
@@ -0,0 +1,41 @@
+import styles from "./HomeCard.module.scss";
+import Image from "next/image";
+
+export default function HomeCard({
+  title,
+  description,
+  img,
+  imgAlt,
+  icon,
+  iconAlt,
+  align,
+}) {
+  return (
+    <div
+      className={`${styles.card} ${align === "left" ? styles.left : styles.right}`}
+    >
+      {/* left side has image and right side has text or vice versa */}
+      <div className={styles.img}>
+        <Image
+          src={img}
+          alt={imgAlt}
+          fill={true}
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+
+      <div className={styles.text}>
+        <div className={styles.header}>
+          <Image src={icon} alt={iconAlt} width={40} height={40} />
+          <h3>{title}</h3>
+        </div>
+
+        {/* THIS IS LONG TEXT */}
+        <div
+          className={styles.description}
+          dangerouslySetInnerHTML={{ __html: description }}
+        />
+      </div>
+    </div>
+  );
+}
