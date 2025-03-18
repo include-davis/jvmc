7eddac467727ee2c5707bbadcb547f14d5c9213a
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2025-03-18 15:08:36 -0700

    Fix bug with persisting mobile drop-down after switching pages

--- app/(pages)/_components/Header/Header.jsx
+++ app/(pages)/_components/Header/Header.jsx
@@ -30,6 +30,7 @@ export default function Header() {
                 key={href}
                 href={href}
                 className={styles.mobileHeaderLinks}
+                onClick={() => setMenuOpen(!menuOpen)}
               >
                 {label}
               </Link>
