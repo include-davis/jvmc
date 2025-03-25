fc73b831f43f7da43eb7618476829867082757c4
C3viche <28328104+C3viche@users.noreply.github.com>
2025-03-25 14:49:47 -0700

    bring changes from 41-cms-integration

--- app/(pages)/clinic-schedule/page.jsx
+++ app/(pages)/clinic-schedule/page.jsx
@@ -1,22 +1,33 @@
-"use client";
 import styles from "./page.module.scss";
 import Image from "next/image";
-import data from "../_data/clinic-schedule.json";
-import { MdInsertInvitation } from "react-icons/md";
-import { IoMdInformationCircle } from "react-icons/io";
+import ClinicCardFallbackData from "../_data/clinic-schedule.json";
+import ClinicSchedulesCard from "../_components/ClinicSchedulesCard/ClinicSchedulesCard";
 
-export default function ClinicSchedule() {
+async function getClinicSchedulesCards() {
+  return ClinicCardFallbackData;
+}
+
+export default async function ClinicSchedule() {
+  const data = await getClinicSchedulesCards();
+
   return (
     <main>
       <div className={styles.page}>
         <div className={styles.header}>
-          <h1>{data.title}</h1>
-          <h4>{data.subtitle}</h4>
+          <h1>Calendar & Clinic Schedules</h1>
+          <h4>
+            Need an appointment? Check availability and schedule one with our
+            clincs now.
+          </h4>
         </div>
 
         <div className={styles.gradientContainer}>
           <div className={styles.gradient}>
-            <Image src={data.gradient} alt={data.gradient_alt} fill={true} />
+            <Image
+              src="/images/clinicScheduleGradient.png"
+              alt="Background Gradient"
+              fill={true}
+            />
           </div>
         </div>
 
@@ -30,11 +41,11 @@ export default function ClinicSchedule() {
           </div>
           <div className={styles.key}>
             <h4>Calendar Key</h4>
-            {Object.entries(data.key).map(([key, clinic]) => (
+            {data.map((clinic, key) => (
               <div key={key} className={styles.keyElem}>
                 <div
                   className={styles.circ}
-                  style={{ backgroundColor: `var(${clinic.color})` }}
+                  style={{ backgroundColor: `${clinic.color}` }}
                 ></div>
                 <p className={styles.title}>{clinic.title}</p>
               </div>
@@ -43,34 +54,8 @@ export default function ClinicSchedule() {
         </div>
 
         <div className={styles.infocards}>
-          {Object.entries(data.info).map(([key, clinic]) => (
-            <div key={key} className={styles.card}>
-              <div className={styles.info}>
-                <h4>{clinic.title}</h4>
-                <p>{clinic.paragraph}</p>
-                <div className={styles.tm}>
-                  <div className={styles.text}>
-                    <MdInsertInvitation size={24} color="var(--emerald)" />
-                    <p>{clinic.time}</p>
-                  </div>
-                  <div className={styles.text}>
-                    <IoMdInformationCircle size={24} color="var(--emerald)" />
-                    <p>{clinic.message}</p>
-                  </div>
-                </div>
-              </div>
-              {clinic.appt === "button" ? (
-                <a
-                  target="_blank"
-                  href="https://www.genderhealthcenter.org"
-                  className="btn"
-                >
-                  Visit GHC Website
-                </a>
-              ) : (
-                <p className={styles.appt}>{clinic.appt}</p>
-              )}
-            </div>
+          {data.map((clinic, key) => (
+            <ClinicSchedulesCard key={key} clinic={clinic} />
           ))}
         </div>
       </div>
