c43cdeb60f3f6a8a9250bb811318b6a087c775d2
C3viche <28328104+C3viche@users.noreply.github.com>
2025-04-02 11:01:36 -0700

    added basic CSP to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' https://apis.google.com https://www.gstatic.com https://maps.googleapis.com 'unsafe-inline' 'unsafe-eval';
+  script-src-elem 'self' https://apis.google.com https://www.gstatic.com 'unsafe-inline';
+  style-src 'self' https://fonts.googleapis.com 'unsafe-inline';
+  img-src 'self' https://*.googleusercontent.com data:;
+  font-src 'self' https://fonts.gstatic.com;
+  frame-src https://calendar.google.com https://www.google.com/maps https://maps.google.com https://*.google.com;
+  connect-src 'self' https://*.googleapis.com;
+  `;
+
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, " ").trim(),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
