58cc83fc9c3a0047a865b377e9a1af7b0941e777
C3viche <28328104+C3viche@users.noreply.github.com>
2025-04-01 17:24:07 -0700

    formatted email + don't send alert on invalid input

--- app/(pages)/api/sendEmail/route.js
+++ app/(pages)/api/sendEmail/route.js
@@ -40,12 +40,30 @@ export async function POST(req) {
       from: process.env.EMAIL_FROM,
       to: process.env.EMAIL_TO, // EDIT WITH ACTUAL CONTACT FORM EMAIL
       subject: subject,
-      text: `
-                Name: ${firstName} ${lastName}
-                Email: ${email}
-                Message: ${message}
-            `, // Can edit with name/email in body if needed
-      // HTML is another option for formatting
+      html: `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Contact Form Submission</title>
+        </head>
+        <body style="font-family: sans-serif; line-height: 1.6; color: #333; background-color: #f4f4f4; padding: 20px;">
+          <div style="background-color: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+            <h1 style="color: #01887a; border-bottom: 2px solid #eee; padding-bottom: 10px; ">Contact Form Submission</h1>
+            <p style="margin-bottom: 15px;">You have received a new message from your contact form.</p>
+            <div style="background-color: #f9f9f9; padding: 16px; border-radius: 4px; margin-bottom: 20px; display: flex; flex-direction: column; gap: 16px;">
+              <div style="margin-bottom: 16px"><strong style="display: block; margin-bottom: 4px;">Name:</strong> ${firstName} ${lastName}</div>
+              <div style="margin-bottom: 16px"><strong style="display: block; margin-bottom: 4px;">Email:</strong> ${email}</div>
+              <div style="margin-bottom: 16px"><strong style="display: block; margin-bottom: 4px;">Message:</strong> ${message}</div>
+            </div>
+            <div style="margin-top: 20px; text-align: center; color: #777;">
+              This is an automated email sent from your website's contact form.
+            </div>
+          </div>
+        </body>
+        </html>
+      `,
     };
 
     await transporter.sendMail(mailOptions);
