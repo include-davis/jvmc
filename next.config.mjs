d6f538d3e0de7f25ac6c51e472d1617efd760217
C3viche <28328104+C3viche@users.noreply.github.com>
2025-04-17 19:22:43 -0700

    add cms to CSP whitelist to accept cors

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,10 @@ const cspHeader = `
   script-src 'self' https://apis.google.com https://www.gstatic.com https://maps.googleapis.com 'unsafe-inline' 'unsafe-eval';
   script-src-elem 'self' https://apis.google.com https://www.gstatic.com 'unsafe-inline';
   style-src 'self' https://fonts.googleapis.com 'unsafe-inline';
-  img-src 'self' https://*.googleusercontent.com https://res.cloudinary.com data:;
+  img-src 'self' https://*.googleusercontent.com https://res.cloudinary.com data: https://cms.joanviterimemorialclinic.com;
   font-src 'self' https://fonts.gstatic.com data:;
-  frame-src https://calendar.google.com https://www.google.com/maps https://maps.google.com https://*.google.com;
-  connect-src 'self' https://*.googleapis.com;
+  frame-src https://calendar.google.com https://www.google.com/maps https://maps.google.com https://*.google.com https://cms.joanviterimemorialclinic.com;
+  connect-src 'self' https://*.googleapis.com https://cms.joanviterimemorialclinic.com;
   `;
 
 const nextConfig = {
